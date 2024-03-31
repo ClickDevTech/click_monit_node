915b15a1023488595b02cfb86280c6adb3268006
pavelpakhnutow <pavelpakhnutow@gmail.com>
2024-03-31 20:35:34 +0300

    oop

--- click_monit_node.js
+++ click_monit_node.js
@@ -119,11 +119,15 @@ socket.on('periodSelected', async (selectedPeriod) => {
         // Добавляем усредненные значения в массив
         avg.push(avgStats);
 
+        avgStats = null;
+
     }
 
     avg[0].name = name;
     console.log('Все данные в таблице system_stats отправлены');
     await socket.emit('charts', avg);
+
+    avg = null;
 
 });
 
